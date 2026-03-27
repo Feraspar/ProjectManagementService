1771a60a29762310e470dfc761c2fba2f8f1254f
misha <misha@MIKHAIL>
2026-03-27 18:12:41 +0700

    Add entities

---
+++ ProjectService.Core/Entities/ProjectDocument.cs
@@ -0,0 +1,54 @@
+﻿namespace ProjectService.Core.Entities
+{
+	using System;
+
+	/// <summary>
+	/// Project document entity;
+	/// </summary>
+	public class ProjectDocument
+	{
+		#region Public Properties
+
+		/// <summary>
+		/// Project document Id.
+		/// </summary>
+		public Guid Id { get; set; }
+
+		/// <summary>
+		/// Project Id.
+		/// </summary>
+		public Guid ProjectId { get; set; }
+
+		/// <summary>
+		/// Project document name.
+		/// </summary>
+		public string FileName { get; set; } = null!;
+
+		/// <summary>
+		/// Project document name in storage.
+		/// </summary>
+		public string StoragedFileName { get; set; } = null!;
+
+		/// <summary>
+		/// Project document type.
+		/// </summary>
+		public string ContentType { get; set; } = null!;
+
+		/// <summary>
+		/// Project document size.
+		/// </summary>
+		public long Size { get; set; }
+
+		/// <summary>
+		/// Project document uploaded time.
+		/// </summary>
+		public DateTimeOffset UploadedAt { get; set; }
+
+		/// <summary>
+		/// Project to which the document relates.
+		/// </summary>
+		public Project Project { get; set; } = null!;
+
+		#endregion Public Properties
+	}
+}
