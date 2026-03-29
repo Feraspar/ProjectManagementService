5d0c20314000357b380959d176c4750e0879f4bb
misha <misha@MIKHAIL>
2026-03-29 22:33:06 +0700

    Add DTO for file storage service

---
+++ ProjectManagementService.Core/Contracts/Request/SaveFileRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace ProjectManagementService.Core.Contracts.Request
+{
+	using System;
+
+	/// <summary>
+	/// Request for saving a file to storage.
+	/// </summary>
+	/// <param name="ProjectId">Project identifier.</param>
+	/// <param name="FileName">Original file name.</param>
+	/// <param name="ContentType">File content type.</param>
+	/// <param name="Content">File content stream.</param>
+	public record SaveFileRequest(
+
+		Guid ProjectId,
+
+		string FileName,
+
+		string ContentType,
+
+		Stream Content
+	);
+}
---
+++ ProjectManagementService.Core/Contracts/Response/SavedFileResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace ProjectManagementService.Core.Contracts.Response
+{
+	/// <summary>
+	/// Response with saved file metadata.
+	/// </summary>
+	/// <param name="FileName">Original file name.</param>
+	/// <param name="StoredFileName">Stored file name.</param>
+	/// <param name="Path">Stored file path.</param>
+	/// <param name="ContentType">File content type.</param>
+	/// <param name="Size">File size in bytes.</param>
+	public record SavedFileResponse(
+
+		string FileName,
+
+		string StoredFileName,
+
+		string Path,
+
+		string ContentType,
+
+		long Size
+	);
+}
